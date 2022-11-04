b7f484d5bf960203a05e7c4a36a2b5b0200458e3
Lucas Nhimi <lucasnhimi@gmail.com>
2022-11-04 13:43:25 -0300

    Initial Code

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>Dash Page</h1>;
+}
---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import NavLink from './nav-link';
+import './global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html className='bg-gray-800 text-gray-100'>
+      <head></head>
+      <body>
+        <header className='border-b p-4'>
+          <nav className='space-x-4'>
+            <NavLink href='/'>Home</NavLink>
+            <NavLink href='/dashboard'>Dashboard</NavLink>
+          </nav>
+        </header>
+        <main className='p-4'>{children}</main>
+      </body>
+    </html>
+  );
+}
